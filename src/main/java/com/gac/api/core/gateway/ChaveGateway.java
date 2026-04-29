6419014362320ec22f94050ec9791d1a5db1df64
Pedro Alberto <pedroalbertompontes@gmail.com>
2026-04-28 23:43:38 -0300

    add: domain classes gateways

---
+++ src/main/java/com/gac/api/core/gateway/ChaveGateway.java
@@ -0,0 +1,15 @@
+package com.gac.api.core.gateway;
+
+import com.gac.api.core.domain.Chave;
+import java.util.List;
+import java.util.Optional;
+
+public interface ChaveGateway {
+    Chave salvar(Chave chave);
+
+    List<Chave> listarTodos();
+
+    Optional<Chave> buscarPorId(Long id);
+
+    void excluir(Long id);
+}
